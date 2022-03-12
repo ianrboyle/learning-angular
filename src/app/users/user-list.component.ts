4f53c32e8863881e34ed59269e6af4b3b2ba2b29
Ian Boyle <irboyle21@gmail.com>
2022-03-12 10:39:29 -0500

    adds a list of users. uses *ngIf and *ngFor

--- src/app/users/user-list.component.ts
+++ src/app/users/user-list.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'pm-users',
-    templateUrl: './user-list.component.html'
+    templateUrl: './user-list.component.html',
+    
 })
 
 export class UserListComponent {
-
+  pageTitle: string = "User list";
+  users: any[] = [
+    {
+        "userId": 1,
+        "fullName": "Mohamad Lawand",
+        "email": "mohamad@email.com",
+        "phone": "1231123",
+        "country": "lebanon"
+    },
+    {
+        "userId": 2,
+        "fullName": "Richard Feynman",
+        "email": "richard@email.com",
+        "phone": "333333",
+        "country": "US"
+    },
+    {
+        "userId": 3,
+        "fullName": "Neil Degrasse Tyson",
+        "email": "neil@email.com",
+        "phone": "44444444",
+        "country": "US"
+    }
+]
 }
