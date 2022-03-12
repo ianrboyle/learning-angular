6c2f5b256cdded480dc9a27e8384ab9710d8c785
Ian Boyle <irboyle21@gmail.com>
2022-03-12 11:15:15 -0500

    adds filter functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HelloWorldComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './users/user-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { UserListComponent } from './users/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/user-list.component.ts
+++ src/app/users/user-list.component.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core'
-
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './user'
 @Component({
     selector: 'pm-users',
     templateUrl: './user-list.component.html',
     
 })
 
-export class UserListComponent {
+export class UserListComponent implements OnInit {
   pageTitle: string = "User list";
-  users: any[] = [
+  users: IUser[] = [
     {
         "userId": 1,
         "fullName": "Mohamad Lawand",
         "email": "mohamad@email.com",
-        "phone": "1231123",
+        "phone": 1231123,
         "country": "lebanon"
     },
     {
         "userId": 2,
         "fullName": "Richard Feynman",
         "email": "richard@email.com",
-        "phone": "333333",
+        "phone": 333333,
         "country": "US"
     },
     {
         "userId": 3,
         "fullName": "Neil Degrasse Tyson",
         "email": "neil@email.com",
-        "phone": "44444444",
+        "phone": 44444444,
         "country": "US"
     }
 ];
@@ -36,4 +36,27 @@ showNumber:boolean = false;
 showPhone(): void {
         this.showNumber = !this.showNumber;
     };
+
+
+  private _listFilter: string = '';
+
+get listFilter(): string {
+    return this._listFilter;
+}
+set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredUsers = this.performFilter(value);
+}
+
+filteredUsers: IUser[] = [];
+
+ngOnInit(): void {
+    this.listFilter = '';
+};
+
+performFilter(filterBy: string): IUser[] {
+    filterBy = filterBy.toLowerCase();
+    return this.users.filter((user: IUser) =>
+            user.fullName.toLowerCase().includes(filterBy));
+};
 }
